9ba326c792ddf9b6a59c7d502d60d156f7482492
Luan Monteiro <luan.monteiro@zrobank.com.br>
2022-10-29 11:10:28 -0300

    feat:  API retornará apenas items ultilizados

--- CryptoCurrenciesTracking/hooks/apiCrypto.tsx
+++ CryptoCurrenciesTracking/hooks/apiCrypto.tsx
@@ -1,47 +1,55 @@
-import axios from "axios"
-import moment from "moment"
-import { SampleData } from "../assets/data/sampleData";
-
+import axios from "axios";
+import moment from "moment";
+import _ from "lodash";
 //para cada item respondido da API fazer um push no valor convertido
 const formatSparkline = (numbers: any[]) => {
-    const sevenDaysAgo = moment().valueOf();
-    let formattedSparkline = numbers.map((item: any,index: number) => {
-        return{
-            timestamp: sevenDaysAgo + (index + 1 )* 3600,//converte para segundos
-            value: item,
-        } 
-    })
-    let newDate = (new Date(formattedSparkline[0].timestamp).toLocaleDateString())
+  const sevenDaysAgo = moment().subtract(7, "minutes").unix();
+  let formattedSparkline = numbers.map((item: string, index: number) => {
+    return {
+      timestamp: sevenDaysAgo + (index + 1) * 3600, //converte para segundos
+      value: item,
+    };
+  });
 
-    return formattedSparkline
-}
+  return formattedSparkline;
+};
+//função para trocar o objeto retornado pela API pelo formatSparkline criado acima
+const fixFormatData = (data: any) => {
+  let formattedResponse: any = [];
 
-const fixFormatData = (data:any) => {
-    let formattedResponse:any = [];
+  data.forEach((item: any) => {
+    const formattedSparkline = formatSparkline(item.sparkline_in_7d.price);
 
-    data.forEach((item: any) => {
-        const formattedSparkline = formatSparkline(item.sparkline_in_7d.price)
+    const formattedItem = {
+      ...item,
+      sparkline_in_7d: {
+        price: formattedSparkline,
+      },
+    };
 
-        const formattedItem = {
-            ...item,
-            sparkline_in_7d:{
-                price: formattedSparkline
-            }
-        }
-             
-        formattedResponse.push(formattedItem)
-    });
+    formattedResponse.push(formattedItem);
+  });
 
-    return formattedResponse
-}
+  return formattedResponse;
+};
 
 export const getCryptoData = async () => {
-    try{        //chamada da api
-        const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=150&page=1&sparkline=true&price_change_percentage=7d")
-        const result = response.data;
-        const formatedResponse = fixFormatData(result);
-        return formatedResponse
-    }catch(err){
-        return SampleData
-    }
-}
+  try {
+    //chamada da api
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d"
+    );
+    const result = response.data;
+    const formatedResponse = fixFormatData(result);
+
+    //Remove os items sem utilidades do json
+    const simplifiedResponse :any= 
+      _.map(formatedResponse, (formatedResponse) =>//, "symbol", "current_price", "price_change_percentage_7d_in_currency","image"
+        _.pick(formatedResponse, ["id", "symbol","name", "current_price", "price_change_percentage_7d_in_currency","image","sparkline_in_7d"])
+      )
+    ;
+    return simplifiedResponse;
+  } catch (err) {
+    console.error(err);
+  }
+};
